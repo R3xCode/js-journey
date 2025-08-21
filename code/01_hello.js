51038d26441b556ca9d274f810b17c8d7ee38c78
R3xCode <neogabhinaba@gmail.com>
2025-08-21 17:27:50 +0530

    Add new comments

--- code/01_hello.js
+++ code/01_hello.js
@@ -1,3 +1,8 @@
-console.log("Hello, this is Abhi.");
-console.log("Welcome to the world of JavaScript!");
+console.log("Hello, This is Abhi");
+// Outputs a greeting message.
+
+console.log("Welcome to the JavaScript world!");
+// Outputs a welcome message.
+
 console.log("Let's start coding!");
+// Outputs a message to encourage coding.
--- code/02_variables.js
+++ code/02_variables.js
@@ -1,24 +1,28 @@
 const acctId = 8762862;
-/*
-accountId cannot be updated
-because it is a constant
-*/
+// accId cannot be updated because it is a constant.
 
 let accEmail = "xyz@gmail.com";
+// let allows reassignment.
 
 var accPassword = "28bak81lk";
 /*
-Prefer not to use var
-because of issue in block scope and functional scope
+var is not recommended in modern JavaScript due to its quirks. So, we prefer not to use var because of issue in block scope and functional scope.
 */
 
 accCity = "Guwahati";
+/*
+accCity is not declared with let or var, so it becomes a global variable & this is not recommended as it can lead to unexpected behavior.
+*/
+
 let accountState;
+// accountState is declared but not initialized.
 
 accEmail = "axoi@gmail.com";
 accPassword = "52790028261";
 accCity = "Tezpur";
 
 console.log(acctId);
+// Outputs the constant account ID.
 
 console.table([acctId, accEmail, accPassword, accCity, accountState]);
+// Outputs the variables in a table format for better readability.
