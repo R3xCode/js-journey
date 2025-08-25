c0e7299d674ccf82bf5ac46ac50f0a3af7801b0f
R3xCode <neogabhinaba@gmail.com>
2025-08-25 22:34:00 +0530

    Refactor console log comments for consistency

--- code/01_hello.js
+++ code/01_hello.js
@@ -1,8 +1,3 @@
-console.log("Hello, This is Abhi");
-// Outputs a greeting message.
-
-console.log("Welcome to the JavaScript world!");
-// Outputs a welcome message.
-
-console.log("Let's start coding!");
-// Outputs a message to encourage coding.
+console.log("Hello, This is Abhi"); // Outputs a greeting message
+console.log("Welcome to the JavaScript world!"); // Outputs a welcome message
+console.log("Let's start coding!"); // Outputs a message to encourage coding
